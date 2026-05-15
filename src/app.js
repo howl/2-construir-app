20ecb52d9b72e8c3b1363b9573c678866a04e80f
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-15 10:53:11 +0200

    implement routes structure
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,69 +1,16 @@
 const express = require('express');
 
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("<h1>Práctica JWT</h1>");
-})
-
-
-
-app.get('/api', (req, res) => {
-
-    res.status(200).json({
-        posts: [
-            {
-                id: 0,
-                title: "Primer post",
-                username: 'Pepe'
-            },
-            {
-                id: 1,
-                title: "Segundo post",
-                username: 'Ana'
-            }
-        ]
-    })
-
-})
-
-
-
-app.get('/login', (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <title></title>
-            </head>
-            <body>
-                <form method="POST" action="/auth">
-                
-                    <div>Nombre de Usuario: <input type='text' name='username'> </div>
-                    <div>Contraseña: <input type='text' name='paswword'> </div>
-                    <div> <input type='submit' value='Iniciar sesión'  >  </div>
-                
-                </form>
-            </body>
-        </html>
-    `);
-})
-
-
-app.post('/auth', (req, res) => {
-    const { username, password } = req.body;
-
-    const user = { username: username }
-})
-
-
-
-
-
+app.use('/', require('./routes/public'));
+app.use('/api', require('./routes/public'));
+app.use('/login', require('./routes/public'));
+app.use('/auth', require('./routes/public'));
 
 app.listen(port, () => {
-    console.log('a la escucha del ', port)
-})
+  console.log('a la escucha del ', port)
+});
---
+++ src/routes/public.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send("<h1>Práctica JWT</h1>");
+});
+
+router.get('/api', (req, res) => {
+  res.status(200).json({
+    posts: [
+      {
+        id: 0,
+        title: "Primer post",
+        username: 'Pepe'
+      },
+      {
+        id: 1,
+        title: "Segundo post",
+        username: 'Ana'
+      }
+    ]
+  });
+});
+
+router.get('/login', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title></title>
+      </head>
+      <body>
+        <form method="POST" action="/auth">
+
+          <div>Nombre de Usuario: <input type='text' name='username'> </div>
+          <div>Contraseña: <input type='text' name='paswword'> </div>
+          <div> <input type='submit' value='Iniciar sesión'  >  </div>
+
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+router.post('/auth', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = { username: username };
+});
+
+module.exports = router;
